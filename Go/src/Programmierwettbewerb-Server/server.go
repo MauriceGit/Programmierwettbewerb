634524bb1cab0c94e6c8fd73c85b89947fb116f5
mauricegit <maurice.tollmien@gmail.com>
2016-06-24 09:48:05 +0200

    WORKING

--- Go/src/Programmierwettbewerb-Server/server.go
+++ Go/src/Programmierwettbewerb-Server/server.go
@@ -625,9 +625,8 @@ func updateServerFromGit() {
 }
 
 func restartServer() {
-    //restart := exec.Command("sleep 5; ./Programmierwettbewerb-Server &")
-    restart := exec.Command("/bin/bash", "test.sh")
-    //restart.Dir = "./"
+    restart := exec.Command("/bin/bash", "restartServer.sh")
+    restart.Dir = "../"
     //restart.SysProcAttr.Setpgid = true
     if err := restart.Start(); err != nil {
         Logf(LtDebug, "error on restarting the server: %v\n", err)
