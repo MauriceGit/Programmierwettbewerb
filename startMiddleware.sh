167ade2f3d1b8f22ee30cea1823a8164c4bda0c0
mauricegit <maurice.tollmien@gmail.com>
2016-07-06 13:24:40 +0200

    Oh, forgot to edit the startMiddleware

--- startMiddleware.sh
+++ startMiddleware.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 #./bin/Programmierwettbewerb-Middleware -bot=./BotPython/script.py -numBots=10 -name=Destroyer3
-./bin/Programmierwettbewerb-Middleware -bot=./BotPython/script.py -numBots=1 -name=Maurice -connection=ws://localhost:8080/middleware/
-#./bin/Programmierwettbewerb-Middleware -bot=./BotPython/script.py -numBots=1 -name=dummy
+#./bin/Programmierwettbewerb-Middleware -bot=./BotPython/script.py -numBots=1 -name=Maurice -connection=ws://localhost:8080/middleware/
+./bin/Programmierwettbewerb-Middleware -bot=./BotPython/script.py -numBots=1 -name=dummy
