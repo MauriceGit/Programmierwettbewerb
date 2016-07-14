05a1cc07fd8b3d6c91f704debc2dd56d747cb43a
mauricegit <maurice.tollmien@gmail.com>
2016-07-14 20:44:11 +0200

    check on players now comes with color and some formatting :)

--- checkOnPlayers.sh
+++ checkOnPlayers.sh
@@ -1,1 +1,40 @@
-ssh pwb-agario@cagine.fh-wedel.de "cat Programmierwettbewerb/server.log | grep -E \"The player\" | grep -v \"dummy\""
+#!/bin/bash
+
+list=$(ssh pwb-agario@cagine.fh-wedel.de "cat Programmierwettbewerb/server.log | grep -E \"The player\" | grep -v \"dummy\"" | uniq -f 15 -c)
+
+for l in "$list"
+do
+    break
+    #echo -e "\e[31m$l\e[0m"
+    #echo "player: $(sed -e 's/(\(.*\))$l/\1/')"
+    successfullName=$(echo "$l" | awk -v FS="(player |can be)" '{print $2}')
+
+    echo -e "TEST \e[31m$successfullName\e[0m"
+
+    echo "$l"
+
+done
+
+while read -r line; do
+    successfullName=$(echo "$line" | awk -v FS="(player | can be)" '{print $2}' | grep -v "not allowed" | grep -v "not be associated")
+    svn=$(echo "$line" | awk -v FS="(svn-repos |. At)" '{print $2}')
+    time=$(echo "$line"| awk -v FS="(At: |CEST)" 'BEGIN{IGNORECASE = 1}{print $2}')
+    failedName=$(echo "$line" | awk -v FS="(The player | is not)" '{print $2}' | grep -v "not be associated")
+
+    RED='\033[0;31m'
+    GREEN='\033[0;32m'
+    NC='\033[0m' # No Color
+
+    if [ -n "$successfullName" ]
+    then
+        echo -e "Player ${GREEN}$successfullName\t${NC} can be associated with ${GREEN}$svn${NC} at $time"
+        #echo "$successfullName $svn $time"
+    else
+        if [ -n "$failedName" ]
+        then
+            echo -e "Player ${RED}$failedName\t${NC} failed at                        $time"
+            #echo -e "Player $failedName\t can be associated with pwb_05 at $time"
+        fi
+    fi
+
+done <<< "$list"
