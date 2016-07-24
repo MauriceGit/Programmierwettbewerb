9fd70d33a136baf7c362e5a888df208b383d5211
mauricegit <maurice.tollmien@gmail.com>
2016-07-24 13:07:29 +0200

    Nearly doubled the view-window size, so you are not accidently eaten by very big blobs

--- Go/src/Programmierwettbewerb-Server/server.go
+++ Go/src/Programmierwettbewerb-Server/server.go
@@ -1201,7 +1201,7 @@ func (app* Application) startUpdateLoop() {
                 }
 
                 center = Muls(center, 1.0 / float32(len(bot.Blobs)))
-                var windowDiameter float32 = 30.0 * float32(math.Log(float64(completeMass))) - 20.0
+                var windowDiameter float32 = 50.0 * float32(math.Log(float64(completeMass))) - 20.0
 
                 bot.ViewWindow = ViewWindow{
                     Position:   Sub(center, Vec2{ windowDiameter / 2.0, windowDiameter / 2.0 }),
