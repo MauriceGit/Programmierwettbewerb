fad6a0537ef58b531931667192b2f1d61b1c5a43
mauricegit <maurice.tollmien@gmail.com>
2016-09-05 19:36:26 +0200

    checkOnPlayers.sh

--- checkOnPlayers.sh
+++ checkOnPlayers.sh
@@ -1,29 +1,29 @@
 #!/bin/bash
 
-list=$(ssh pwb-agario@cagine.fh-wedel.de "cat Programmierwettbewerb/server.log | grep -E \"The player\" | grep -v \"dummy\"")
+list=$(ssh pwb-agario@cagine.fh-wedel.de "cat Programmierwettbewerb/server.log")
 tmpFile="tmpFile"
 
+function parseValue(){
+    value="$2"
+    line=$(echo "$1" | sed -s "s/^.*$2/$2/")
+    echo $(echo $line | awk -v FS="($value=\"|\".)" '{print $2}')
+}
+
+function parseTime(){
+    echo $(echo "$1" | awk -v FS="(^| Line)" '{print $1}')
+}
+
 while read -r line; do
-    successfullName=$(echo "$line" | grep -v "not allowed" | grep -v "not be associated" | awk -v FS="(player | can be)" '{print $2}')
-    svn=$(echo "$line" | awk -v FS="(svn-repos |. At)" '{print $2}')
-    time=$(echo "$line"| awk -v FS="(At: |CEST)" 'BEGIN{IGNORECASE = 1}{print $2}')
-    failedName=$(echo "$line" | awk -v FS="(The player | is not)" '{print $2}' | grep -v "not be associated")
 
-    RED='\033[0;31m'
-    GREEN='\033[0;32m'
-    NC='\033[0m' # No Color
-
-    if [ -n "$successfullName" ]
+    if [ -n "$(echo "$line" | grep "NEW_BOT")" ]
     then
-        echo -e "Player ${GREEN}$successfullName\t${NC} can be associated with ${GREEN}$svn${NC} at $time"
-    else
-        if [ -n "$failedName" ]
-        then
-            echo -e "Player ${RED}$failedName\t${NC} failed at                        $time"
-        fi
+        name=$(parseValue "$line" "NAME")
+        svn=$(parseValue "$line" "SVN")
+        ip=$(parseValue "$line" "IP")
+        time=$(parseTime "$line")
+        echo -e "New Bot: $name, svn: $svn, ip: $ip, time: $time."
+        continue
     fi
 
 done <<< "$list"
 
-
-cat $tmpFile | uniq -f 10 -c
